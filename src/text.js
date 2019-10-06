18e84312ff06f123b346b1e57d4d143f015169b0
Datalcoholic <rechezuria@gmail.com>
2019-10-06 15:37:32 -0400

    texto de los pasos

---
+++ src/text.js
@@ -0,0 +1,13 @@
+const textSteps =[
+    {step:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, quasi ad. Tenetur dicta tempore provident incidunt, iste consectetur. Assumenda explicabo, qui quibusdam modi perferendis ratione voluptatem quod error reiciendis asperiores.'},
+    {step:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, quasi ad. Tenetur dicta tempore provident incidunt, iste consectetur. Assumenda explicabo, qui quibusdam modi perferendis ratione voluptatem quod error reiciendis asperiores.'},
+    {step:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, quasi ad. Tenetur dicta tempore provident incidunt, iste consectetur. Assumenda explicabo, qui quibusdam modi perferendis ratione voluptatem quod error reiciendis asperiores.'},
+    {step:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, quasi ad. Tenetur dicta tempore provident incidunt, iste consectetur. Assumenda explicabo, qui quibusdam modi perferendis ratione voluptatem quod error reiciendis asperiores.'},
+    {step:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, quasi ad. Tenetur dicta tempore provident incidunt, iste consectetur. Assumenda explicabo, qui quibusdam modi perferendis ratione voluptatem quod error reiciendis asperiores.'},
+    {step:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, quasi ad. Tenetur dicta tempore provident incidunt, iste consectetur. Assumenda explicabo, qui quibusdam modi perferendis ratione voluptatem quod error reiciendis asperiores.'},
+    {step:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, quasi ad. Tenetur dicta tempore provident incidunt, iste consectetur. Assumenda explicabo, qui quibusdam modi perferendis ratione voluptatem quod error reiciendis asperiores.'},
+    {step:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, quasi ad. Tenetur dicta tempore provident incidunt, iste consectetur. Assumenda explicabo, qui quibusdam modi perferendis ratione voluptatem quod error reiciendis asperiores.'},
+
+] 
+
+module.exports = textSteps 
